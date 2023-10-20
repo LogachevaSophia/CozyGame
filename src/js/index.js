d87a32a71ecae7caaa2923bbc0e7e3a5b5127083
София <48216113+LogachevaSophia@users.noreply.github.com>
2023-10-20 22:29:21 +0300

    m

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-canvas.width = 1024;
-canvas.height = 576;
+canvas.width = window.screen.width;
+canvas.height = window.screen.height;
 
-context.fillStyle = "red";
-context.fillRect(0, 0, canvas.width, canvas.height);
+// context.fillStyle = "red";
+// context.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
   constructor({ position, image }) {
@@ -55,20 +55,17 @@ function animation() {
     playerImage.height
   );
   if (keys.w.pressed) backGround.position.y += 3;
-  else {
-    if (keys.s.pressed) backGround.position.y -= 3;
-    else {
-      if (keys.a.pressed) backGround.position.x += 3;
-      else {
-        if (keys.d.pressed) backGround.position.x -= 3;
-      }
-    }
-  }
+
+  if (keys.s.pressed) backGround.position.y -= 3;
+
+  if (keys.a.pressed) backGround.position.x += 3;
+
+  if (keys.d.pressed) backGround.position.x -= 3;
 }
 animation();
 
 window.addEventListener("keydown", (e) => {
-  // console.log(e.key)
+  console.log(e.key)
   switch (e.key) {
     case "w":
       keys.w.pressed = true;
